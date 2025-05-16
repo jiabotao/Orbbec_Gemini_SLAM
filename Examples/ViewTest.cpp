b1688b14287692a95b5febe4394c569a8254cc36
jiabotao <724270650@qq.com>
2025-05-16 15:48:34 +0800

    add orbbec demo

--- Examples/ViewTest.cpp
+++ Examples/ViewTest.cpp
@@ -2,25 +2,35 @@
 #include "utils.hpp"
 
 int main() {
-    ob::Context ctx;
-    std::shared_ptr<ob::DeviceList> deviceList = ctx.queryDeviceList();
-    if(deviceList->deviceCount() == 0) {
-        std::cerr << "未找到设备!" << std::endl;
-        return -1;
-    }
-    std::shared_ptr<ob::Device> device = deviceList->getDevice(0);
-    auto depthModeList = device->getDepthWorkModeList();
-    std::cout << "depthModeList size: " << depthModeList->count() << std::endl;
-    for(int i = 0; i < depthModeList->count(); i++) {
-        //std::cout << "depthModeList[" << i << "]: " << (*depthModeList)[i].name << std::endl;
-    }
+        // Create a pipeline with default device
+    ob::Pipeline pipe;
+    // Configure which streams to enable or disable for the Pipeline by creating a Config
+    std::shared_ptr<ob::Config> config = std::make_shared<ob::Config>();
+    // Get the ir_left camera configuration list
+    std::shared_ptr<ob::StreamProfileList> irLeftProfiles = pipe.getStreamProfileList(OB_SENSOR_IR_LEFT);
+    std::shared_ptr<ob::StreamProfile> irLeftProfile = irLeftProfiles->getProfile(26);
+    config->enableStream(irLeftProfile->as<ob::VideoStreamProfile>());
+    std::shared_ptr<ob::StreamProfileList> irRightProfiles = pipe.getStreamProfileList(OB_SENSOR_IR_RIGHT);
+    std::shared_ptr<ob::StreamProfile> irRightProfile = irRightProfiles->getProfile(26);
+    config->enableStream(irRightProfile->as<ob::VideoStreamProfile>());
+    pipe.start(config);
+    while(true) {
+        // Wait for up to 100ms for a frameset in blocking mode.
+        std::shared_ptr<ob::FrameSet> frameSet = pipe.waitForFrames(200);
+        if(frameSet == nullptr) {
+            continue;
+        }
 
-    std::shared_ptr<ob::Pipeline> pipeline = std::make_shared<ob::Pipeline>(device);
-    std::shared_ptr<ob::StreamProfileList> OB_SENSOR_IR_LEFT_ProfileList = pipeline->getStreamProfileList(OB_SENSOR_IR_LEFT);
-    std::cout << "OB_SENSOR_IR_LEFT_ProfileList size: " << OB_SENSOR_IR_LEFT_ProfileList->count() << std::endl;
-    for(int i = 0; i < OB_SENSOR_IR_LEFT_ProfileList->count(); i++) {
-        auto colorProfile = OB_SENSOR_IR_LEFT_ProfileList->getProfile(i)->as<ob::VideoStreamProfile>();
-        std::cout << "color profile: " << colorProfile->width() << "x" << colorProfile->height() << " @ " << colorProfile->fps() << "fps" << std::endl;
+        // Get the data of left and right IR
+        std::shared_ptr<ob::Frame> leftFrame  = frameSet->getFrame(OB_FRAME_IR_LEFT);
+        std::shared_ptr<ob::Frame> rightFrame = frameSet->getFrame(OB_FRAME_IR_RIGHT);
+        if(leftFrame == nullptr || rightFrame == nullptr) {
+            continue;
+        }
+        
+        std::shared_ptr<ob::VideoFrame> leftIRvideoFrame = leftFrame->as<ob::VideoFrame>();
+        std::shared_ptr<ob::VideoFrame> rightIRvideoFrame = rightFrame->as<ob::VideoFrame>();
+        std::cout << "left_time: " << leftIRvideoFrame->timeStamp() << ", right_time: " << rightIRvideoFrame->timeStamp() << std::endl;
     }
 
 
