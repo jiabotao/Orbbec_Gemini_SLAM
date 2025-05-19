251eb8544439e8c97b2bb17639d7c93343db5b57
jiabotao <724270650@qq.com>
2025-05-19 20:32:00 +0800

    change sdk

--- Examples/OrbbecDemo/OrbbecDoubleIRView.cpp
+++ Examples/OrbbecDemo/OrbbecDoubleIRView.cpp
@@ -1,50 +1,132 @@
-#include <libobsensor/ObSensor.hpp>
-#include "utils.hpp"
+#include "window.hpp"
+
+#include "libobsensor/ObSensor.hpp"
+#include "libobsensor/hpp/Error.hpp"
 #include <mutex>
 
-#define ESC 27
+const int maxDeviceCount = 9;
 
-void handleFrameset(std::shared_ptr<ob::FrameSet> frameset);
+std::vector<std::shared_ptr<ob::Frame>> frames;
+std::shared_ptr<ob::Frame>              colorFrames[maxDeviceCount];
+std::shared_ptr<ob::Frame>              depthFrames[maxDeviceCount];
+std::shared_ptr<ob::Frame>              irFrames[maxDeviceCount];
+std::mutex                              frameMutex;
 
-std::shared_ptr<ob::Context> ctx;
-std::shared_ptr<ob::Config> config;
-std::shared_ptr<ob::Device> device;
+void StartStream(std::vector<std::shared_ptr<ob::Pipeline>> pipes);
+void StopStream(std::vector<std::shared_ptr<ob::Pipeline>> pipes);
 
-std::mutex frameMutex;
+int main(int argc, char **argv) try {
+    // Create a Context
+    ob::Context ctx;
 
-int main() 
-{
-    ctx = std::make_shared<ob::Context>();
-    auto config = std::make_shared<ob::Config>();
-    std::shared_ptr<ob::DeviceList> devices = ctx->queryDeviceList();
-    device = devices->getDevice(0);
-    device->setBoolProperty(OB_PROP_LASER_CONTROL_INT, 0);
+    // Query the list of connected devices
+    auto devList = ctx.queryDeviceList();
+    // Get the number of connected devices
+    int devCount = devList->deviceCount();
 
-    
-    std::shared_ptr<ob::Pipeline> pipe = std::make_shared<ob::Pipeline>(device);
-    if (!pipe) {
-        std::cerr << "XXXXXXXXXXXXX." << std::endl;
-        return 1;
+    // traverse the device list and create a pipe
+    std::vector<std::shared_ptr<ob::Pipeline>> pipes;
+    for(int i = 0; i < devCount; i++) {
+        // Get the device and create the pipeline
+        auto dev  = devList->getDevice(i);
+        auto pipe = std::make_shared<ob::Pipeline>(dev);
+        pipes.push_back(pipe);
     }
-    std::shared_ptr<ob::StreamProfileList> irLeftProfiles  = pipe->getStreamProfileList(OB_SENSOR_IR_LEFT);
-    std::shared_ptr<ob::StreamProfileList> irRightProfiles = pipe->getStreamProfileList(OB_SENSOR_IR_RIGHT);
-    if(irLeftProfiles == nullptr && irRightProfiles == nullptr) 
-    {
-        std::cerr << "左IR或右IR配置有误." << std::endl;
-        return 0;
-    }
-    //26号为 1280*800分辨率 5帧
-    std::shared_ptr<ob::StreamProfile> irLeftProfile  = irLeftProfiles ->getProfile(0);
-    config->enableStream(irLeftProfile->as<ob::VideoStreamProfile>());
-    std::shared_ptr<ob::StreamProfile> irRightProfile = irRightProfiles->getProfile(0);
-    config->enableStream(irRightProfile->as<ob::VideoStreamProfile>());
-    pipe->start(config,handleFrameset);
 
+    // Open depth and color streams for all devices
+    StartStream(pipes);
+
+    // Create a window for rendering and set the resolution of the window
+    Window app("MultiDeviceViewer", 1280, 720, RENDER_GRID);
+
+    while(app) {
+        // Render a set of frame in the window, here will render the depth, color or infrared frames of all devices, RENDER_GRID
+        // means that all frames will be rendered in a grid arrangement
+        frames.clear();
+        {
+            std::lock_guard<std::mutex> lock(frameMutex);
+            int                         i = 0;
+            for(auto pipe: pipes) {
+                if(colorFrames[i] != nullptr) {
+                    frames.emplace_back(colorFrames[i]);
+                }
+
+                if(depthFrames[i] != nullptr) {
+                    frames.emplace_back(depthFrames[i]);
+                }
+
+                if(irFrames[i] != nullptr) {
+                    frames.emplace_back(irFrames[i]);
+                }
+                i++;
+            }
+        }
+
+        app.addToRender(frames);
+    }
+
+    frames.clear();
+    StopStream(pipes);
 
     return 0;
 }
+catch(ob::Error &e) {
+    std::cerr << "function:" << e.getName() << "\nargs:" << e.getArgs() << "\nmessage:" << e.getMessage() << "\ntype:" << e.getExceptionType() << std::endl;
+    exit(EXIT_FAILURE);
+}
 
-void handleFrameset(std::shared_ptr<ob::FrameSet> frameset) {
-    std::lock_guard<std::mutex> lock(frameMutex);
-    std::cout << "333333333" << std::endl;
+void StartStream(std::vector<std::shared_ptr<ob::Pipeline>> pipes) {
+    int i = 0;
+    for(auto &&pipe: pipes) {
+        std::shared_ptr<ob::Config> config = std::make_shared<ob::Config>();
+        // Get the depth camera configuration list
+        auto                                    depthProfileList = pipe->getStreamProfileList(OB_SENSOR_IR_LEFT);
+        std::shared_ptr<ob::VideoStreamProfile> depthProfile     = nullptr;
+        if(depthProfileList) {
+            // Open the default profile of Depth Sensor, which can be configured through the configuration file
+            depthProfile = std::const_pointer_cast<ob::StreamProfile>(depthProfileList->getProfile(OB_PROFILE_DEFAULT))->as<ob::VideoStreamProfile>();
+        }
+        config->enableStream(depthProfile);
+
+        // Get the color camera configuration list
+        try {
+            auto                                    colorProfileList = pipe->getStreamProfileList(OB_SENSOR_COLOR);
+            std::shared_ptr<ob::VideoStreamProfile> colorProfile     = nullptr;
+            if(colorProfileList) {
+                // Open the default profile of Color Sensor, which can be configured through the configuration file
+                colorProfile = std::const_pointer_cast<ob::StreamProfile>(colorProfileList->getProfile(OB_PROFILE_DEFAULT))->as<ob::VideoStreamProfile>();
+            }
+            config->enableStream(colorProfile);
+        }
+        catch(ob::Error &e) {
+            std::cerr << "Current device is not support color sensor!" << std::endl;
+        }
+
+        // Start the pipeline and pass in the configuration
+        pipe->start(config, [i](std::shared_ptr<ob::FrameSet> frameSet) {
+            std::lock_guard<std::mutex> lock(frameMutex);
+            if(frameSet->colorFrame()) {
+                colorFrames[i] = frameSet->colorFrame();
+            }
+            if(frameSet->irFrame()) {
+                depthFrames[i] = frameSet->irFrame();
+            }
+        });
+        i++;
+    }
+}
+
+void StopStream(std::vector<std::shared_ptr<ob::Pipeline>> pipes) {
+    int i = 0;
+    for(auto &&pipe: pipes) {
+        if(colorFrames[i])
+            colorFrames->reset();
+        if(depthFrames[i])
+            depthFrames->reset();
+        if(irFrames[i])
+            irFrames->reset();
+        // stop the pipeline
+        pipe->stop();
+        i++;
+    }
 }
