0c5013247adc75f16d3cad1fef6953a8883b1488
jiabotao <724270650@qq.com>
2025-05-20 00:35:19 +0800

    Init

--- include/IMUProcessor.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "IMUFrameQueueContainer.hpp"
-#include "IMU.hpp"
-#include <stdlib.h>
-
-class IMUProcessor {
-    public:
-        void IMUCallBack(IMUFrameQueueContainer& imuFrameQueueContainer);
-};
